cf1d76e27c07e04effc5719cadf497a537d20a61
Ross <rossco.king78@outlook.com>
2020-08-17 05:06:12 +0930

    updated

--- Family.py
+++ Family.py
@@ -1,31 +1,39 @@
 
 class Family:
-    def __init__(self, first, last, parent, grand, gen, male):
+    def __init__(self, first, last, parent, grand, great, gen, male):
         self.first = first
         self.last = last
         self.parent = parent
         self.grand = grand
+        self.great = great
         self.gen = gen
         self.male = male
     
     def fullName(self):
         return '{} {}'.format(self.first, self.last)
 
-       
-Sharon = Family('Sharon', 'Brett', 'Kitty', 'Unknown', 2, False)
-Ross = Family('Ross', 'King', 'Sharon', 'Kitty', 3, True)
-Bret = Family('Bret', 'Champ', 'Sharon', 'Kitty', 3, True)
-Kelly = Family('Kelly', 'Goode', 'Sharon', 'Kitty', 3, False)
-Harrison = Family('Harrison', 'Champ', 'Bret', 'Sharon', 4, True)
+Kitty = Family('Kitty', 'Blank', 'A.Foster', 'Unknown', 'Unknown3', 1, False)    
+Sharon = Family('Sharon', 'Blank', 'Kitty', 'A_Foster', 'Unknown', 2, False)
+Ross = Family('Ross', 'King', 'Sharon', 'Kitty', 'Unknown', 3, True)
+Bret = Family('Bret', 'Blank', 'Sharon', 'Kitty', 'Unknown', 3, True)
+Kelly = Family('Kelly', 'Blank', 'Sharon', 'Kitty', 'Unknown',3, False)
+Harrison = Family('Harrison', 'Blank', 'Bret', 'Sharon', 'Kitty', 4, True)
+Remy = Family('Remy', 'Blank', 'Kelly', 'Sharon', 'Kitty', 4, False)
+Poppy = Family('Poppy', 'Blank', 'Kelly', 'Sharon', 'Kitty', 4, False)
+Ellis = Family('Ellis', 'Blank', 'Kelly', 'Sharon','Kitty', 4, False)
+Willow = Family('Willow', 'Blank', 'Remy', 'Kelly', 'Sharon', 5, False)
+Taj = Family('Taj', 'Blank', 'Remy', 'Kelly', 'Sharon', 5, True)
 
-Phil = Family('Phil', 'Brett', 'Kitty', 'Unknown', 2, True)
-Lee = Family('Lee', 'Bret', 'Phil','Kitty', 3, True )
+Phil = Family('Phil', 'Blank', 'Kitty', 'A_Foster', 'Unknown2', 2, True)
+Lee = Family('Lee', 'Blank', 'Phil','Kitty',  'Unknown', 3, True )
+James = Family('James', 'Blank', 'Phil', 'Kitty',  'Unknown', 3, True)
+Robbie = Family('Robbie', 'Blank', 'Phil', 'Kitty',  'Unknown', 3, True)
+Jess = Family('Jess', 'Blank', 'Phil', 'Kitty',  'Unknown', 3, False)
 
-print(Ross.fullName())  
+Mick = Family('Mick', 'Blank', 'Kitty', 'A.Foster', 'Unknown2', 2, True)
+Lou = Family('Louise', 'Blank', 'Mick', 'Kitty', 'A.Foster', 3, False)
+Kev = Family('Kevin', 'Blank', 'Kitty', 'A.Foster', 'Unkown2', 2, True)
 
-#def checkRel(*args, **kwargs:
- #   print(args)
-#    print(kwargs)
 
 def chekFam(one, two):
     if one.parent == two.parent and two.male == True:
@@ -33,31 +41,39 @@ def chekFam(one, two):
     elif one.parent == two.parent and two.male == False:
             print('Sister')
     elif one.gen == two.gen and one.parent != two.parent:
-        print('Cousins')
-    elif one.parent == two.first and one.male == True:
+        print('Cousin')
+    elif one.first == two.parent and one.male == True:
         print('Son')
     elif one.parent == two.first and one.male != True:
         print('Daughter')
-    elif one.grand == two.parent and two.male == True:
+    elif one.grand == two.parent and two.first != one.parent and two.male == True:
         print('Uncle')
-    elif one.grand == two.parent and two.male == False:
+    elif one.grand == two.parent and two.male == False and  two.first != one.parent and two.male != True:
         print('Aunt')
-    elif one.parent == two.grand and one.male == True:
+    elif one.parent == two.grand and two.male == True:
          print('Nephew')
-    elif one.parent == two.grand and one.male != True:
+    elif one.parent == two.grand and two.male != True:
         print('Niece')
-    
-    
-        
-print(Phil.fullName())
-chekFam(Ross, Phil)
-chekFam(Ross, Lee)
-chekFam(Ross, Kelly)
-chekFam(Ross, Sharon)
-chekFam(Kelly, Sharon)
-chekFam(Kelly, Bret)
-chekFam(Harrison, Kelly)
-chekFam(Phil, Ross)
+    elif one.grand == two.great:
+        print('Second cousin')
+    elif one.parent == two.first and two.male != True:
+        print('Mum')
+    elif one.first == two.parent and one.male == True:
+        print('Dad')
+    elif one.grand == two.first and two.male == False:
+        print('Nan')
+    elif one.grand == two.first and two.male == True:
+        print('GrandDad')
+    elif one.parent == two.great and two.male == False:
+        print('Great neice')
+    elif one.parent == two.great and two.male == True:
+        print('Great nephew')
+
+
+chekFam(Mick, Remy)   
+  
+
+
 
 
 
